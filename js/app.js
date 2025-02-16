b89c96f296fb9ad23129804847e5655443050ed8
FL232323 <Fosterleboeuf23@gmail.com>
2025-02-16 10:36:11 -0500

    Complete createPropTypeChart function in app.js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+                    backgroundColor: '#dc3545'
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Prop Type Performance'
+                }
+            },
+            scales: {
+                x: { stacked: true },
+                y: { stacked: true }
+            }
+        }
+    });
+}
